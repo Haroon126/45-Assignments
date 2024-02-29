538dcbdda60bf213b919dee9419b2b56d2de6e2c
[Haroon Memon] <[haroonmemon432@gmail.com]>
2024-02-29 12:42:50 +0500

    secondt commit

---
+++ Assignment # 4/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// Print the quote and the name of its author
+let famousQuote = "The greatest glory in living lies not in never falling, but in rising every time we fall";
+console.log(`Nelson Mandela once said ,  "${famousQuote}" `);
---
+++ Assignment # 4/index.ts
@@ -0,0 +1,5 @@
+// Print the quote and the name of its author
+
+let famousQuote :string = "The greatest glory in living lies not in never falling, but in rising every time we fall"
+console.log(`Nelson Mandela once said ,  "${famousQuote}" `);
+
